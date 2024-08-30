2f3b274d394fe62d0758c767d3f7610a93ed7eeb
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2024-08-30 12:04:52 +0200

    WIP : parachute

---
+++ personnel/ParaClub/ParaClub/Plane.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaClub
+{
+    public class Plane
+    {
+        public int _x;
+
+
+        public Plane(int x) 
+        {
+            _x = x;
+
+            //Dessiner la premiere fois l'avion
+            Console.SetCursorPosition(_x, 0);
+            foreach (string line in view)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        private string[] view =
+        {
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
+
+        public void MovePlane()
+        {
+            //Déplacer la zone
+            Console.MoveBufferArea(_x, 0, view[0].Length, 6, _x + 1, 0);
+
+            _x++;
+        }
+    }
+}
---
+++ personnel/ParaClub/ParaClub/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ParaClub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Windows settings 
+            Console.WindowHeight = Config.SCREEN_HEIGHT;
+            Console.WindowWidth = Config.SCREEN_WIDTH;
+
+            //Instantiate plane
+            Plane plane = new Plane(0);
+
+            while (true)
+            {
+                plane.MovePlane();
+
+                //Speed
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
