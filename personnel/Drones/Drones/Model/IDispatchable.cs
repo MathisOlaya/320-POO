6477fbbda7405f5b7c0f67c7161226b9803b4949
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2024-10-04 09:24:05 +0200

    feat(POO): Créer l'interface IDispatchable

---
+++ personnel/Drones/Drones/Model/IDispatchable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones
+{
+    public interface IDispatchable
+    {
+        List<Box> AllBoxes { get; }
+        void AddBox(Box box);
+        void RemoveBox(Box box);
+    }
+}
