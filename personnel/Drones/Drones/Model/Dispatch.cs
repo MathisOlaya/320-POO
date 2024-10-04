3e91312b584e8e74782f4793faf778a34c85e536
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2024-10-04 09:25:42 +0200

    feat(POO): Implémenter l'interface dans Dispatch

--- personnel/Drones/Drones/Model/Dispatch.cs
+++ personnel/Drones/Drones/Model/Dispatch.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace Drones
 {
-    public class Dispatch
+    public class Dispatch : IDispatchable
     {
-        
+        public List<Box> AllBoxes => throw new NotImplementedException();
+
+        public void AddBox(Box box)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveBox(Box box)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
